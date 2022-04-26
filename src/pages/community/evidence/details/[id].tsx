8bb1093dc2396a3f8809f4f4bd136d5609704d2b
bishal neupane <neupanebishal07@gmail.com>
2022-04-26 11:31:32 +0200

    add comment section to evidence and notification page for admin and volunteers

--- src/pages/community/evidence/details/[id].tsx
+++ src/pages/community/evidence/details/[id].tsx
@@ -1,6 +1,17 @@
-import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Input,
+  Text,
+} from '@chakra-ui/react'
 import React from 'react'
 
+//TODO: When clicked on user avatar should open user profile
+//TODO: We might use the carousel to show the evidence images
 function EvidenceDetails() {
   return (
     <Box>
@@ -40,6 +51,53 @@ function EvidenceDetails() {
             Dan Abrohmov
           </Text>
         </Flex>
+        <Box>
+          <Text fontWeight="bold" my="2">
+            Tagged Volunteers
+          </Text>
+          <AvatarGroup size="md">
+            <Avatar name="Ryan Florence" src="https://bit.ly/ryan-florence" />
+            <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
+            <Avatar name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
+            <Avatar
+              name="Prosper Otemuyiwa"
+              src="https://bit.ly/prosper-baba"
+            />
+            <Avatar name="Christian Nwamba" src="https://bit.ly/code-beast" />
+          </AvatarGroup>
+        </Box>
+      </Box>
+
+      <Box my="6">
+        <Flex>
+          <Input placeholder="Write your comments here" variant="filled" />
+          <Button mx="4">Post</Button>
+        </Flex>
+
+        <HStack my="4">
+          <Text bg="gray.200" color="gray.500" w="80%" borderRadius="5px" p="2">
+            Please update your description keep it short so that everyone can
+            understand it well
+          </Text>
+          <Avatar
+            name="Dan Abrahmov"
+            size="lg"
+            cursor="pointer"
+            src="https://bit.ly/dan-abramov"
+          />
+        </HStack>
+
+        <HStack my="4">
+          <Avatar
+            name="Ryan Florence"
+            size="lg"
+            src="https://bit.ly/ryan-florence"
+          />
+
+          <Text bg="gray.200" color="gray.500" w="80%" borderRadius="5px" p="2">
+            okay give me some time
+          </Text>
+        </HStack>
       </Box>
     </Box>
   )
