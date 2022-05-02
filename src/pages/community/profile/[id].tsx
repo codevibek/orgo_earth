9aa78f5ec81b0fa7095e56679b63504253fe2693
bishal neupane <neupanebishal07@gmail.com>
2022-05-02 19:46:01 +0545

    update

--- src/pages/community/profile/[id].tsx
+++ src/pages/community/profile/[id].tsx
@@ -15,15 +15,15 @@ import {
   AiFillTwitterCircle,
 } from 'react-icons/ai'
 import { EditProfileDrawer } from '../../../components/EditProfileDrawer'
-import { useGetUserProfile } from '../../../data/hooks/query/useGetUserProfile'
+// import { useGetUserProfile } from '../../../data/hooks/query/useGetUserProfile'
 import { useIsMe } from '../../../data/hooks/useIsMe'
-import { useStore } from '../../../data/store'
+// import { useStore } from '../../../data/store'
 
 function Profile() {
   const { onOpen, isOpen, onClose } = useDisclosure()
   const router = useRouter()
   const profileUserId = router.query.id as string
-  const { isLoading, data } = useGetUserProfile(profileUserId)
+  // const { isLoading, data } = useGetUserProfile(profileUserId)
 
   const isMe = useIsMe(profileUserId)
 
