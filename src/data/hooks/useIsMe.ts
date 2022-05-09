d8445c0914defa3a7995915ee9cec308bcdf030b
bishal neupane <neupanebishal07@gmail.com>
2022-05-09 08:43:25 +0545

    fix

--- src/data/hooks/useIsMe.ts
+++ src/data/hooks/useIsMe.ts
@@ -2,5 +2,5 @@ import { useUserData } from './useUserData'
 
 export function useIsMe(username: string) {
   const userData = useUserData()
-  return userData.username === username
+  return userData?.username === username
 }
