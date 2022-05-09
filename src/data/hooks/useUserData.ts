44975b5e8f5c267b364329d8dac8e8ee27a90f39
bishal neupane <neupanebishal07@gmail.com>
2022-05-09 08:29:25 +0545

    fix userdata update on path change

--- src/data/hooks/useUserData.ts
+++ src/data/hooks/useUserData.ts
@@ -1,11 +1,14 @@
+import { useRouter } from 'next/router'
 import { useMemo } from 'react'
 import { isServer } from '../utils/isServer'
 import { User } from './mutations/useRegister'
 
 export const useUserData = () => {
+  const router = useRouter()
   const userData: User = useMemo(() => {
     if (isServer) return null
+    console.log(router.pathname)
     return JSON.parse(localStorage.getItem('userData'))
-  }, [])
+  }, [router.pathname])
   return userData
 }
