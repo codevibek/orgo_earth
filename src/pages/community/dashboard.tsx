25e92eff34c4f73a00f18eeac3fb8eacecbae58f
bishal neupane <neupanebishal07@gmail.com>
2022-05-11 09:39:14 +0545

    update

--- src/pages/community/dashboard.tsx
+++ src/pages/community/dashboard.tsx
@@ -14,7 +14,7 @@ function Dashboard() {
   const { data: Tasks, isLoading } = useGetCommunityTasks(userData?._id)
 
   return (
-    <Box height="90vh" overflowY="auto">
+    <Box>
       <Text fontSize={{ base: 'xl', sm: '2xl' }} fontWeight="bold">
         Welcome to Community Dashboard
       </Text>
