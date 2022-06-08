79ae5dc8e270236db25549b09b9e33ebc23edc10
bishal neupane <neupanebishal07@gmail.com>
2022-06-08 13:08:12 +0545

    update the license and clear the readme

--- src/data/hooks/useShowContacInfo.ts
+++ src/data/hooks/useShowContacInfo.ts
@@ -1,13 +1,6 @@
-// we need to hide the contact information for both
-// the user and the community account if the current logged
-// in user is either a guest or a volunteer only show the
-// contact information for the community account
-// also if current profile is logged in users then
-// show it regardless of the user type
 
 import { useRouter } from 'next/router'
 import { useIsMe } from './useIsMe'
-// import { useUserData } from './useUserData'
 
 export function useShowContactInfo() {
   const router = useRouter()
